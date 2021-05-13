db0474e9b6a3cbb9d2fcfb61b582dafd1d09a425
Abhinav Rao <abhinavrao941@gmail.com>
2021-05-13 19:00:18 +0530

    warnings removed

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -33,7 +33,7 @@ const colors = {
     dark: "#343a40",
 };
 
-const shadow = {
+export const shadow = {
     "very-light": "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
     light: " 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
     medium: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
