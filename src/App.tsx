b0428c6617b1961588fa12530efab7f8ddef1505
Abhinav Rao <abhinav.rao@gslab.com>
2021-05-15 18:27:42 +0530

    hot reload fix 1. useTheme removed 2. react-script changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import { ThemeProvider } from "styled-components";
-import { createBrowserHistory } from "history";
-import {Router} from "react-router-dom"
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 
-import "antd/dist/antd.css";
+import 'antd/dist/antd.css';
 
-import { darkTheme, lightTheme } from "utils/theme";
-import { useDarkMode } from "components/useTheme";
+import { lightTheme } from 'utils';
 
 export const history = createBrowserHistory();
 
 function App() {
-    const [theme /* toggleTheme */, , componentMounted] = useDarkMode();
-    const themeMode = theme === "light" ? lightTheme : darkTheme;
-    if (!componentMounted) {
-        return <div />;
-    }
     return (
-        <ThemeProvider theme={themeMode}>
-    <Router history={history}>
-    </Router>
-
+        <ThemeProvider theme={lightTheme}>
+            <Router history={history}>helllos</Router>
         </ThemeProvider>
     );
 }
