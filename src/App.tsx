0810ab8061a5b215578f3921a2b4d6040558e513
Abhinav Rao <abhinav.rao@gslab.com>
2021-05-22 21:20:58 +0530

    delete modal folder fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,13 @@ import 'antd/dist/antd.css';
 import './App.css';
 
 import { lightTheme } from 'utils';
-import SampleModalContainer from 'container/Modals/DeleteModal';
 
 export const history = createBrowserHistory();
 
 function App() {
     return (
         <ThemeProvider theme={lightTheme}>
-            <Router history={history}>
-                <SampleModalContainer loading={false} visible={true} toggleVisibility={() => {}} />
-            </Router>
+            <Router history={history}></Router>
         </ThemeProvider>
     );
 }
